2512d408dd62aeb9503e71e98008d2c990dd984d
Michael Tesfay <75224973+Mika215@users.noreply.github.com>
2021-06-05 01:15:23 +0200

    adding variables folder

---
+++ 01-variables/3-let-or-const.js
@@ -0,0 +1,51 @@
+'use strict';
+console.log('-- begin --');
+
+/* declare and assign variables as described in the comments
+
+  pay close attention to how each variable is used!
+  - is a variable assigned a value when it is declared?
+  - is a variable reassigned later in the script?
+
+  your challenge is to decide whether to use let or to use const
+  - use let if
+    the variable is declared without an assignment
+    or the variable is ever reassigned later in the script
+  - use const if
+    the variable is declared with a value and never reassigned
+
+*/
+
+// declare a variable named aTree, don't assign a value!
+let aTree;
+console.log(aTree);
+
+// assign the value "birch" to the variable aTree
+aTree ='birch';
+console.log(aTree);
+
+// declare a variable named turtle and assign it the name "myrtle"
+ const turtle='myrtle';
+console.log(turtle);
+
+// reassign aTree to "aspen"
+aTree='aspen';
+console.log(aTree);
+
+// declare a variable named aColor with the value "blue"
+let aColor='blue';
+console.log(aColor);
+
+// log the value of turtle
+console.log(turtle);
+
+// reassign aTree to "oak"
+aTree='oak';
+console.log(aTree);
+
+// reassign aColor to "orange"
+aColor='orange';
+console.log(aColor);
+
+
+console.log('-- end --');
---
+++ 01-variables/5-let-or-const.js
@@ -0,0 +1,42 @@
+'use strict';
+console.log('-- begin --');
+
+/* declare and assign variables as described in the comments
+
+  pay close attention to how each variable is used!
+  - is a variable assigned a value when it is declared?
+  - is a variable reassigned later in the script?
+
+  your challenge is to decide whether to use let or to use const
+  - use let if
+    the variable is declared without an assignment
+    or the variable is ever reassigned later in the script
+  - use const if
+    the variable is declared with a value and never reassigned
+
+*/
+
+let furniture='chair';
+console.log(furniture); // should log 'chair'
+
+const building='house';
+console.log(building); // should log 'house'
+
+furniture = 'table';
+console.log(furniture); // should log 'table'
+
+let food;
+console.log(food); // should log undefined
+
+food='apple';
+console.log(food); // should log 'apple'
+
+console.log(building); // should log 'house'
+
+console.log(furniture); // should log 'table'
+
+food=food+', pear';
+console.log(food); // should log 'apple, pear'
+
+
+console.log('-- end --');
